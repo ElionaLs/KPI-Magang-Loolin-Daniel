e7feeb7d841ce0bdc526f5351bc314f3c0716d3b
Carmilla <danielprawirasastra@smkwikrama.sch.id>
2023-03-01 09:03:55 +0700

    commit lagi

--- KPI Level 2/LVL2-2.php
+++ KPI Level 2/LVL2-2.php
@@ -2,10 +2,14 @@
     $employee = array("E001", "E002", "E003", "E004", "E005");
     $name = array("John Doe", "Lyra", "Grace", "Idris", "Gwen");
     $birthdate = array("05-11-1998", "13-01-1998", "21-08-1995", "21-07-1993", "05-09-2000");
+    $birthdateBaru = array("05 November 1998", "13 Januari 1998", "21 Agustus 1995", "21 Juli 1993", "05 September 2000");
+    array_Splice($birthdate,0,5,$birthdateBaru);
     $gender = array("M", "F", "F", "M", "F");
     $genderBaru = array("Male", "Female", "Female", "Male", "Female");
     array_splice($gender,0,5,$genderBaru);
     $salary = array("8000000", "10650000", "3450000", "12300000", "1500000");
+    $salaryBaru = array("RP8.000.000", "RP10.650.000", "RP3.450.000", "RP12.300.000", "RP1.500.000");
+    array_splice($salary,0,5,$salaryBaru);
     
     $tabel = COUNT($employee);
 
