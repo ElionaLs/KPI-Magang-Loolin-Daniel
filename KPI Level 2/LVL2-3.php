26c5051bc2bb6ff45412e68fd571ec373e7bc2b4
Carmilla <danielprawirasastra@smkwikrama.sch.id>
2023-03-01 10:16:37 +0700

    commit

--- KPI Level 2/LVL2-3.php
+++ KPI Level 2/LVL2-3.php
@@ -14,35 +14,50 @@ $tabel = [
         "name" => "John Doe",
         "birthdate" => "05-11-1998",
         "gender" => "M",
-        "salary" => "8000000"
+        "salary" => "8000000",
+        "Hobby" => "H001", "H003",
+        "HobbyID" => "H001",
+        "HobbyName" => "Reading"
     ],
     [
         "employee" => "E002",
         "name" => "Lyra",
         "birthdate" => "13-01-1998",
         "gender" => "F",
-        "salary" => "10650000"
+        "salary" => "10650000",
+        "Hobby" => "H004", "H002", "H003",
+        "HobbyID" => "H002",
+        "HobbyName" => "Yoga"
     ],
     [
         "employee" => "E003",
         "name" => "Grace",
         "birthdate" => "21-08-1995",
         "gender" => "F",
-        "salary" => "3450000"
+        "salary" => "3450000",
+        "Hobby" => "H003", "H002",
+        "HobbyID" => "H003",
+        "HobbyName" => "Shopping"
     ],
     [
         "employee" => "E004",
         "name" => "Idris",
         "birthdate" => "21-07-1993",
         "gender" => "M",
-        "salary" => "12300000"
+        "salary" => "12300000",
+        "Hobby" => "H001",
+        "HobbyID" => "H004",
+        "HobbyName" => "Fishing"
     ],
     [
         "employee" => "E005",
         "name" => "Gwen",
         "birthdate" => "05-09-2000",
         "gender" => "F",
-        "salary" => "1500000"
+        "salary" => "1500000",
+        "Hobby" => "H005",
+        "HobbyID" => "H005",
+        "HobbyName" => "Sleeping"
     ],
 ];
 ?>
@@ -54,6 +69,7 @@ $tabel = [
                 <td>Birthdate</td>
                 <td>Gender</td>
                 <td>Salary Type</td>
+                <td>Hobby</td>
               </tr>
             </thead>
             <tbody>
@@ -69,6 +85,7 @@ $tabel = [
                 <td><?= date('d F Y', strtotime($data['birthdate'])) ?></td>
                 <td><?= $genderBaru ?></td>
                 <td><?= $rupiah ?></td>
+                <td><?= $data  ?></td>
             </tr>
         <?php endforeach; ?>
               </thead>
